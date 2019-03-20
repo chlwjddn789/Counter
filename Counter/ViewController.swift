1c0902269e1fbfe283cb32ccbe360dc7ca84b237
D7703_07 <d7703_07@lulzsec.local>
2019-03-20 16:37:36 +0900

    버튼을 누르면 0에서9까지 반복카운트

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var MyCounter: UILabel!
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MyCounter.text = "0"
     }
 
-
+    @IBAction func buttonpressed(_ sender: Any) {
+        if count < 10 {
+            count = count + 1
+            print(count)
+            MyCounter.text = String(count)
+        }
+        if count == 10 {
+            count = 0
+            MyCounter.text = String(count)
+        }
+    }
+    
 }
 
